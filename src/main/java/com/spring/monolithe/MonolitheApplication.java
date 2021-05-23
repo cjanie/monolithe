65c35df8a0477be1d8a6219a4600d745085b4c20
cjanie <janiechun@hotmail.fr>
2021-05-23 17:48:41 +0000

    init

---
+++ src/main/java/com/spring/monolithe/MonolitheApplication.java
@@ -0,0 +1,13 @@
+package com.spring.monolithe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolitheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonolitheApplication.class, args);
+	}
+
+}
